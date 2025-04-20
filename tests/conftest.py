88fea72b2308d726728b3c20694079ee571a6a7a
Elmin Gulamov <g.elmn@yandex.ru>
2025-04-20 12:08:38 +0400

    add allure steps

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,7 @@ def setup_browser():
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     browser = Browser(Config(driver))
