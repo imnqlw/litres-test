3ee38d78d33943d9ca46828b2775e41bcf327593
Elmin Gulamov <g.elmn@yandex.ru>
2025-04-20 16:05:15 +0400

    test 2.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,34 +4,10 @@ import pytest
 from selenium import webdriver
 
 
-# @pytest.fixture(scope="session", autouse=True)
-# def load_env():
-#     load_dotenv()
-#
-# DEFAULT_BROWSER_VERSION = "100.0"
-#
-# selenoid_login = os.getenv('SELENOID_LOGIN')
-# selenoid_pass = os.getenv('SELENOID_PASS')
-
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    # browser_version = DEFAULT_BROWSER_VERSION
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": browser_version,
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://{selenoid_login}:{selenoid_pass}@selenoid.autotests.cloud/wd/hub",
-    #     options=options)
-    #
-    #browser = Browser(Config(driver))
+
 
 
     browser.config.base_url = 'https://www.litres.ru'
@@ -41,10 +17,5 @@ def setup_browser():
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
     yield browser
-
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
 
     browser.quit()
