f1060ce810b0fc0407241695e1c51ff71801f44b
Elmin Gulamov <g.elmn@yandex.ru>
2025-04-20 12:06:47 +0400

    add allure steps

--- tests/test_litres.py
+++ tests/test_litres.py
@@ -3,25 +3,51 @@ import allure
 from utils.web.home_page import MainPage
 
 
-@allure.step("Открываем главную страницу")
+@allure.tag("WEB")
+@allure.title('Выбор книги')
+@allure.epic('книга')
+@allure.story('Новая книга')
+@allure.feature('Выбор книги')
+@allure.label('owner')
+@allure.severity('high')
 def test_new_book():
     main = MainPage()
     main.select_new_books()
     main.check_new_book()
 
-
+@allure.title('Проверка каталога')
+@allure.epic('Проверка каталога')
+@allure.story('Проверка каталога')
+@allure.feature('Проверка каталога')
+@allure.tag('web')
+@allure.label('owner')
+@allure.severity('high')
 def test_catalog():
     main = MainPage()
     main.select_catalog()
     main.check_catalog()
 
 
+@allure.title('Проверка книги')
+@allure.epic('Проверка книги')
+@allure.story('Проверка книги')
+@allure.feature('Проверка книги')
+@allure.tag('web')
+@allure.label('owner')
+@allure.severity('high')
 def test_check_book():
     main = MainPage()
     main.select_found_book()
     main.check_found_book()
 
 
+@allure.title('Проверка строки поиска')
+@allure.epic('Проверка строки поиска')
+@allure.story('Проверка строки поиска')
+@allure.feature('Проверка строки поиска')
+@allure.tag('web')
+@allure.label('owner')
+@allure.severity('high')
 def test_search():
     main = MainPage()
     main.search_with_filtres()
--- utils/web/home_page.py
+++ utils/web/home_page.py
@@ -3,42 +3,44 @@ from selene import browser, have
 
 
 class MainPage:
-
+    @allure.step('Открытие страницы')
     def open(self):
         browser.open('https://www.litres.ru/')
 
-    @allure.step("Открываем главную страницу")
+    @allure.step('Выбор книги')
     def select_new_books(self):
         browser.open('https://www.litres.ru/')
         browser.element("//a[@class='LowerMenuV2_lowerMenu__item__KBMA4'][contains(text(),'Новинки')]").click()
 
-
+    @allure.step('Открытие каталога')
     def select_catalog(self):
         browser.open('https://www.litres.ru/')
         browser.element('[data-testid="header-catalog-button"]').click()
 
-
+    @allure.step('Поиск книги')
     def select_found_book(self):
         browser.open('https://www.litres.ru/')
         browser.element('.SearchForm_input__qDTKP').click().type('Python').press_enter()
 
-
+    @allure.step('Поиск с фильтрами')
     def search_with_filtres(self):
         browser.open('https://www.litres.ru/')
         browser.element('.SearchForm_input__qDTKP').click().type('Python').press_enter()
         browser.element("//label[contains(text(),'Русский')]//*[name()='svg']").click()
 
-
+    @allure.step('Проверка книги')
     def check_new_book(self):
         browser.element("//span[@itemprop='name'][contains(text(),'Новинки')]").should(have.text('Новинки'))
         return self
 
+    @allure.step('Проверка каталога')
     def check_catalog(self):
         browser.element('[href="/collections/besplatnie-knigi/"]').should(have.text('Бесплатные книги'))
 
+    @allure.step('Проверка книги')
     def check_found_book(self):
         browser.element('.SearchTitle_title__Crpui').should(have.text('Python'))
 
-
+    @allure.step('Проверка поиска книги')
     def check_search(self):
         browser.element('[href="/author/dzheyd-karter/"]').should(have.text('Джейд Картер'))
